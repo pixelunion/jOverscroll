dab928a5b5e9496fd08e478d72a211f5334b1ef1
Jared Norman <hi@jarednorman.ca>
2012-07-24 17:21:03 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# jOverscroll.js
+
+A simple jQuery plugin handling text scrolling on hover. We found it a relatively common occurence that a design might need to accommodate text areas that on rare occasions contain more text than is expected (or fits) in situations where wrapping to the next line is not a good solution. We solve this problem in a few places by letting hiding the overflow and scrolling the text when the mouse hovered it, and having it snap back to normal when the mouse comes off it.
+
+## Usage
+
+The plugin is super easy to use:
+
+    $('#some .selector').jOverscroll();
+
+You can override the default values (these are the defaults):
+
+    $('#some .selector').jOverscroll({ width: 'auto', scrollTime: 500, scrollBackTime: 100 });
+
+And you can chain it:
+
+    $('#some .selector').jOverscroll().css({ color: '#ff00ff' });
+
+## Details
+
+Keep in mind that it implicitly sets two CSS properties on the element(s) you pass it: whitespace to pre, and overflow to hidden. To scroll the text it is animating the text-indent property.
---
+++ jOverscroll.plugin.js
@@ -0,0 +1,48 @@
+/*
+    Copyright (c) 2012 Pixel Union, http://pixelunion.net
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+    LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+    OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+;(function($){
+    $.fn.extend({
+        jOverscroll: function(options) {
+            this.defaultOptions = {
+                width: 'auto',
+                scrollTime: 500,
+                scrollBackTime: 100
+            };
+
+            var settings = $.extend({}, this.defaultOptions, options);
+
+            return this.each(function() {
+                var $this = $(this);
+                $this.css({ 'white-space': 'pre', overflow: 'hidden', width: settings.width });
+                $this.hover(function(){
+                    var difference = $(this).get(0).scrollWidth - $(this).width();
+                    if (difference > 0) {
+                        $(this).stop().animate({ 'text-indent': -difference}, settings.scrollTime);
+                    }
+                }, function(){
+                    $(this).stop().animate({ 'text-indent': 0}, settings.scrollBackTime);
+                });
+            });
+        }
+    });
+})(jQuery);
